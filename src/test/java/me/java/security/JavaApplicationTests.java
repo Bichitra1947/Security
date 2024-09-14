e4f4a17f910db015142d2da86887c7c0f95ebee5
bichitra <bichitramohansngha@gmail.com>
2024-09-14 18:47:33 +0530

    first commit

---
+++ src/test/java/me/java/security/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.java.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
